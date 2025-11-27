1af0d18a0585a1e18ec2c3bd124ac5cf1036fa3e
ArcxExe <net.c0reunity@gmail.com>
2025-11-27 17:56:46 +0400

    Remove Milk class . And move realisation logic in Food abstract class
    Был отказ от использовать Milk . Как пример , и потом на основе него
    создавать другую еду . Поэтому было определено  ,что лучше использовать
    и правильнее мощь абстрактных классов . Поэтому все что было сделано в
    классе Milk . Было перенесено в Food

--- src/main/java/ru/arcxexe/solidfridge/Food/Food.java
+++ src/main/java/ru/arcxexe/solidfridge/Food/Food.java
@@ -1,5 +1,35 @@
 package ru.arcxexe.solidfridge.Food;
 
-public abstract class Food {
-  String name;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public abstract class Food implements Expirable {
+  private String name;
+  private final LocalDate dateCreating;
+  private final LocalDate dateDelay;
+
+  public Food(String name, LocalDate dateCreating, LocalDate dateDelay) {
+    this.name = name;
+    this.dateCreating = dateCreating;
+    this.dateDelay = dateDelay;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public LocalDate getCreateingDate() {
+    return this.dateCreating;
+  }
+
+  @Override
+  public long diffrensBetweenDate() {
+    return ChronoUnit.DAYS.between(this.dateCreating, this.dateDelay);
+  }
+
+  @Override
+  public LocalDate getExpiryDate() {
+    return this.dateDelay;
+  }
+
 }
