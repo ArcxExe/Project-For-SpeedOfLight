ba50543ddbf16b2f6743369275d0e47774e5de80
ArcxExe <net.c0reunity@gmail.com>
2025-11-29 13:42:37 +0400

    Add method in FridgerInfo
    Метод который возвращает информацию о холодильнике

--- src/main/java/ru/arcxexe/solidfridge/FridgerInfo.java
+++ src/main/java/ru/arcxexe/solidfridge/FridgerInfo.java
@@ -1,5 +1,12 @@
 package ru.arcxexe.solidfridge;
 
-public record FridgerInfo(String model , String brand , int elcUsing , int maxCountShelf) {
+import java.util.List;
 
+public record FridgerInfo(String model, String brand, int elcUsing, int maxCountShelf, int maxCapacityShelf) {
+
+  public List<String> getInfo() {
+    return List
+        .of("Model: " + model + "Brand: " + brand + "Electic using: " + Integer.toString(elcUsing) + "Max count shelf:"
+            + Integer.toString(maxCountShelf) + "Max capacity shelfs: " + Integer.toString(maxCapacityShelf));
+  }
 }
