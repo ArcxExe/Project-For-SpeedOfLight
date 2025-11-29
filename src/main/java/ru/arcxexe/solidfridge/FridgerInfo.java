e14a8a0636d658f76c1b52105ed216c5af077193
ArcxExe <net.c0reunity@gmail.com>
2025-11-29 13:51:51 +0400

    Formating result method in FridgerInfo

--- src/main/java/ru/arcxexe/solidfridge/FridgerInfo.java
+++ src/main/java/ru/arcxexe/solidfridge/FridgerInfo.java
@@ -6,7 +6,7 @@ public record FridgerInfo(String model, String brand, int elcUsing, int maxCount
 
   public List<String> getInfo() {
     return List
-        .of("Model: " + model + "Brand: " + brand + "Electic using: " + Integer.toString(elcUsing) + "Max count shelf:"
-            + Integer.toString(maxCountShelf) + "Max capacity shelfs: " + Integer.toString(maxCapacityShelf));
+        .of("Model: " + model + "; Brand: " + brand + "; Electic using: " + Integer.toString(elcUsing) + "; Max count shelf: "
+            + Integer.toString(maxCountShelf) + "; Max capacity shelfs: " + Integer.toString(maxCapacityShelf));
   }
 }
